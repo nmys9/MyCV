2eacdbc495402fae725fdf2e796cb986a74c7b16
nmys9 <141567968+nmys9@users.noreply.github.com>
2023-12-15 09:27:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function fun1(){
    document.getElementById("information").innerHTML= "المعلومات الشخصية";
-   document.getElementById("details").innerHTML= "<ul><li>الاسم: نور مجدي سخل</li>   <li>رقم الجوال:0598745632</li> <li>تاريخ الميلاد:22-1-1990</li> <li>الجنسية: فلسطينية</li><li>الديانة:مسلمة</li><li>البريد الإلكتروني: noorsakhel9@gmail.com</li> <li>الحالة الاجتماعية: عزباء</li> <li>عنوان الاقامة: شارع التعاون، نابلس</li></ul>";
+   document.getElementById("details").innerHTML= "<ul><li>الاسم: نور مجدي سخل</li>   <li>رقم الجوال:0595686355</li> <li>تاريخ الميلاد:22-1-2004</li> <li>الجنسية: فلسطينية</li><li>الديانة:مسلمة</li><li>البريد الإلكتروني: noorsakhel04@gmail.com</li> <li>الحالة الاجتماعية: عزباء</li> <li>عنوان الاقامة: شارع التعاون، نابلس</li></ul>";
 }
 function fun2(){
     document.getElementById("information").innerHTML="المؤهلات العلمية";
-    document.getElementById("details").innerHTML=" <b>شهادة ثانوية عامة فرع التكنولوجي - </b>مدرسة سمير سعد الدين 2008 <br> <b>بكالوريوس في أنظمة المعلومات الحاسوبية </b>- جامعة القدس المفتوحة 2013";
+    document.getElementById("details").innerHTML=" <b>شهادة ثانوية عامة فرع التكنولوجي - </b>مدرسة سمير سعد الدين 2021 <br> <b>بكالوريوس في أنظمة المعلومات الحاسوبية </b>- جامعة القدس المفتوحة";
 }
 function fun3(){
     document.getElementById("information").innerHTML="اللغات";
